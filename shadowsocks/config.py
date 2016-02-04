9e56c746b4290d26f43b839bc491038041bde036
qingran001 <qingran001@gmail.com>
2016-02-04 12:47:50 +0800

    123
    123

---
+++ shadowsocks/config.py
@@ -0,0 +1,26 @@
+import logging
+
+#Config
+MYSQL_HOST = '106.2.113.96'
+MYSQL_PORT = 3306
+MYSQL_USER = 'shadowsocks'
+MYSQL_PASS = '1231512315'
+MYSQL_DB = 'shadowsocks'
+
+MANAGE_PASS = 'passwd'
+#if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+#make sure this port is idle
+MANAGE_PORT = 23333
+#BIND IP
+#if you want bind ipv4 and ipv6 '[::]'
+#if you want bind all of ipv4 if '0.0.0.0'
+#if you want bind all of if only '4.4.4.4'
+SS_BIND_IP = '0.0.0.0'
+SS_METHOD = 'aes-256-cfb'
+
+#LOG CONFIG
+LOG_ENABLE = False
+LOG_LEVEL = logging.DEBUG
+LOG_FILE = '/var/log/shadowsocks.log'
+
